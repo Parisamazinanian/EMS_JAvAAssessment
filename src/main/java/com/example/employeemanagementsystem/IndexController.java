63ef15c6a427712a450b2d0bd2be99be92ec03d3
Parisamazinanian <par.mazinanian@gmail.com>
2022-02-08 12:16:37 +0100

    commenting done

--- src/main/java/com/example/employeemanagementsystem/IndexController.java
+++ src/main/java/com/example/employeemanagementsystem/IndexController.java
@@ -2,7 +2,7 @@ package com.example.employeemanagementsystem;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//for controlling the index.html
 @Controller
 public class IndexController {
     @GetMapping(path="/")
